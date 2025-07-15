84c907a11626bfae7e0c82bbc447e9d3ee37dc97
Ebad Sayed <95970293+ES7@users.noreply.github.com>
2025-07-15 19:46:35 +0530

    Update rope.py

--- rope.py
+++ rope.py
@@ -4,19 +4,15 @@ from typing import Tuple
 
 def precompute_freqs_cis(dim: int, end: int, theta: float) -> torch.Tensor:
     freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
-    t = torch.arange(end, device=freqs.device)  # type: ignore
-    freqs = torch.outer(t, freqs).float()  # type: ignore
-    return torch.polar(torch.ones_like(freqs), freqs)  # complex64
+    t = torch.arange(end, device=freqs.device)
+    freqs = torch.outer(t, freqs).float()
+    return torch.polar(torch.ones_like(freqs), freqs)
 
 
-def apply_rotary_emb(
-    xq: torch.Tensor,
-    xk: torch.Tensor,
-    freqs_cis: torch.Tensor,
-) -> Tuple[torch.Tensor, torch.Tensor]:
-    xq_ = torch.view_as_complex(xq.float().reshape(*xq.shape[:-1], -1, 2)) # (Seq, N_Heads, Head_Dim) --> (Seq, N_Heads, Head_Dim // 2)
-    xk_ = torch.view_as_complex(xk.float().reshape(*xk.shape[:-1], -1, 2)) # (Seq, N_Heads, Head_Dim) --> (Seq, N_Heads, Head_Dim // 2)
-    freqs_cis = freqs_cis[:, None, :] # (Seq, 1, Head_Dim // 2)
-    xq_out = torch.view_as_real(xq_ * freqs_cis).flatten(2) # (Seq, N_Heads, Head_Dim // 2) * (Seq, 1, Head_Dim // 2) --> (Seq, N_Heads, Head_Dim // 2) --> (Seq, N_Heads, Head_Dim)
+def apply_rotary_emb(xq: torch.Tensor, xk: torch.Tensor, freqs_cis: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    xq_ = torch.view_as_complex(xq.float().reshape(*xq.shape[:-1], -1, 2))
+    xk_ = torch.view_as_complex(xk.float().reshape(*xk.shape[:-1], -1, 2))
+    freqs_cis = freqs_cis[:, None, :]
+    xq_out = torch.view_as_real(xq_ * freqs_cis).flatten(2)
     xk_out = torch.view_as_real(xk_ * freqs_cis).flatten(2)
     return xq_out.type_as(xq), xk_out.type_as(xk)
